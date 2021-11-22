9855a2c0495c21d07fc04c5182f11af046530fd5
ybnr1130 <muuckk@gmail.com>
2021-11-22 02:44:05 +0000

    Change test code and Add custom helper full_title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -24,10 +24,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
 
 end
