328874cfa98405322919984a89e282bb87ab7da9
ybnr1130 <muuckk@gmail.com>
2021-11-23 10:25:44 +0000

    change method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   
-  # ページごとの完全なタイトルを返します。
+  # ページごとの完全なタイトルを返す。
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
